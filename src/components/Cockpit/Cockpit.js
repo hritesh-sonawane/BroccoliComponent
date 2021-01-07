eb3776fb9dd3fa9d8b1438ad659ec1e22ef9d407
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-07 20:42:48 +0530

    split app into components: Persons & Cockpit

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Cockpit.css'
+
+const cockpit = props => {
+  const assignedClasses = [];
+  let btnClass = '';
+
+  if (props.showPersons) {
+    btnClass = classes.Green;
+  }
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm a React App!</h1>
+      <p className={assignedClasses.join(' ')}>I'm awesome!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Ninjas
+      </button>
+    </div>
+  );
+}
+
+export default cockpit;
