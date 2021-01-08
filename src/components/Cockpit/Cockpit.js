04af08858a5d28ba1fab3f938eed47c40989b4d2
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-08 19:52:12 +0530

    props: index App Cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -17,7 +17,7 @@ const cockpit = props => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App!</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>I'm awesome!</p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Ninjas
