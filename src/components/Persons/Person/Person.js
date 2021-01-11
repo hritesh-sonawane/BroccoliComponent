6afdc273d0bd3e6ebedc96376c02140f6c24368c
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-11 19:34:27 +0530

    hoc 1 commit

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';   // Fragment is similar to Aux
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 
 // import classes from './Person.css';
@@ -8,13 +8,13 @@ class Person extends Component {
     console.log('[Person.js] rendering..');
 
     return (
-      <Fragment>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </Fragment>
+      </Aux>
     );
   }
 };
