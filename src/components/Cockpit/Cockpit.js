b2d185294f2dde271eec9183b8cee80eaa164224
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-11 23:40:32 +0530

    props chaining issue

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -45,6 +45,7 @@ const cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Ninjas
       </button>
+      <button onClick={props.login}>Log In</button>
     </div>
   );
 }
